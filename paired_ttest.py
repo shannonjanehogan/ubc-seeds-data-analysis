f957defc2458034e1c9fc2542da09484790b090a
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-03-26 16:02:03 -0700

    Run Wilcoxon test on data

--- paired_ttest.py
+++ paired_ttest.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
+import unicodecsv as csv
 
 ## p values, t value, degrees of freedom
 culled_data = pd.read_csv("psych_data.csv")
@@ -52,10 +53,19 @@ def findAfter(before_row):
 
 cleanData()
 cleaned_records = pd.DataFrame.from_dict(cleaned_data)
+keys = cleaned_data[0].keys()
+with open('cleaned_data.csv', 'wb') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(cleaned_data)
+
+# Tutorials followed:
+# https://pythonfordatascience.org/paired-samples-t-test-python/
+# http://pythonfordatascience.org/wilcoxon-sign-ranked-test-python/
 
 print("Pre-cleaned data description\n")
 print(culled_data[['item_per_kilo','weight', 'num_contam']].describe())
-print("Cleaned data description\n")
+print("\nCleaned data description\n")
 print(cleaned_records[['item_per_kilo_before','item_per_kilo_after']].describe())
 
 cleaned_records[['item_per_kilo_before', 'item_per_kilo_after']].plot(kind='box')
@@ -70,6 +80,14 @@ stats.probplot(cleaned_records['item_per_kilo_difference'], plot=plt)
 plt.title('Item Per Kilo Difference Q-Q Plot')
 plt.savefig('item_per_kilo_difference_qq_plot.png')
 
-print(stats.shapiro(cleaned_records['item_per_kilo_difference']))
+## Decided not to use paired ttest due to data violating the assumption of the ttest
+# print(stats.shapiro(cleaned_records['item_per_kilo_difference']))
+# print(stats.ttest_rel(cleaned_records['item_per_kilo_before'], cleaned_records['item_per_kilo_after']))
 
-print(stats.ttest_rel(cleaned_records['item_per_kilo_before'], cleaned_records['item_per_kilo_after']))
+cleaned_records[['item_per_kilo_before','item_per_kilo_after']].describe()
+cleaned_records['item_per_kilo_difference'] = cleaned_records['item_per_kilo_before'] - cleaned_records['item_per_kilo_after']
+cleaned_records['item_per_kilo_difference'][cleaned_records['item_per_kilo_difference']==0]
+print("\n Wilcoxon test on item per kilo difference")
+print(stats.wilcoxon(cleaned_records['item_per_kilo_difference']))
+print("\n Wilcoxon test on item per kilo before and after")
+print(stats.wilcoxon(cleaned_records['item_per_kilo_before'], cleaned_records['item_per_kilo_after']))
